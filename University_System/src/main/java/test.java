2b19eba758ae2605e1a5a8139dce8a513599e868
Bu-Sung <qntjd4680@gmail.com>
2021-11-06 14:09:39 +0900

    print

--- University_System/src/main/java/test.java
+++ University_System/src/main/java/test.java
@@ -16,6 +16,7 @@ public class test {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Test");
+        System.out.println("Hi");
     }
     
 }
